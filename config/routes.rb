c8d3e8a63bc15ac5e2598a3154ac05495284e542
Gabriel YR <gabriel.yarleque.r@uni.pe>
2023-12-13 09:42:29 -0500

    EF

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rottenpotatoes::Application.routes.draw do
+    resources :movies
+    # map '/' to be a redirect to '/movies'
+    
+    root :to => redirect('/movies')
+    get '/movies/search_tmdb'
+  end
+  
